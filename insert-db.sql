6d6a2643fd2f174c907a50332b13c62173605121
UrbainTowanou <Townouurbain16@gmail.com>
2026-07-28 11:27:20 +0200

    ajout des scripts init/insert/select pour geoDB

---
+++ insert-db.sql
@@ -0,0 +1,8 @@
+USE geoDB;
+
+INSERT INTO departement (num, nom, prefecture) VALUES
+('01', 'Ain', 'Bourg-en-Bresse'),
+('02', 'Aisne', 'Laon'),
+('75', 'Paris', 'Paris'),
+('69', 'Rhône', 'Lyon'),
+('13', 'Bouches-du-Rhône', 'Marseille');
